b00499a1aae6fad996140183ed12a2631b4a2350
Alexey Zaitsev <apirator.framework@gmail.com>
2025-09-28 16:40:05 +0200

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-export { withBackoff } from "./with-backoff";
-export { TBackoffOptions, TError, TRetryOptions } from "./types";
-export { withNetworkBackoff } from "./with-network-backoff";
-export { withInternalServerErrorBackoff } from "./with-internal-server-error-backoff";
-export { withConnectionErrorMessageBackoff } from "./with-connection-error-message-backoff";
-export { createDelayList } from "./create-delay-list";
-export {
-  WithBackoff,
-  WithNetworkBackoff,
-  WithInternalServerErrorBackoff,
-  WithConnectionErrorMessageBackoff,
-} from "./decorators";
+export * from "./with-backoff";
+export * from "./types";
+export * from "./with-network-backoff";
+export * from "./with-internal-server-error-backoff";
+export * from "./with-connection-error-message-backoff";
+export * from "./create-delay-list";
+export * from "./decorators";
